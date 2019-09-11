e39bcf678627cdcb74a10aedae476cac3a23cc47
Val Astraverkhau <val@mizustudio.ca>
2019-09-11 14:56:41 -0400

    Update alpine

--- liveness.sh
+++ liveness.sh
@@ -3,8 +3,10 @@
 # path to script
 path=$(cd -P -- "$(dirname -- "$0")" && printf '%s\n' "$(pwd -P)")
 
+clamdscan --stream --config-file=/app/conf/clamd.conf ${path}/eicar.com
+
 # clamd
-if clamdscan --config-file=/app/conf/clamd.conf ${path}/eicar.com | grep -q 'Infected files: 1'; then
+if clamdscan --allmatch --config-file=/app/conf/clamd.conf ${path}/eicar.com | grep -q 'Infected files: 1'; then
     echo "clamd running successfully"
     exit 0
 else
